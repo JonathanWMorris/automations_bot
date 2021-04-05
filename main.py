becfe40ba21964146547167825a63ad165585659
Jonathan Morris <jonathan_wellington@icloud.com>
2021-04-05 08:48:48 -0700

    - using elif instead of if to separate the commands - added -!help command - added -!animals command 	- This only works when the animal bot is in the channel - added a catch all error message when "-!" is used 	- For this to be effective it should always be the last elif statement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import requests
 import base64
+import random
 
 client = discord.Client()
 
@@ -18,18 +19,16 @@ async def on_message(message):
     if message.content.startswith('-!hello'):
         await message.channel.send('Hello!')
 
-    if message.content.startswith('-!cat'):
+    elif message.content.startswith('-!cat'):
         result = requests.get('https://api.thecatapi.com/v1/images/search')
         result_json = result.json()
         image = result_json[0]
         image_url = image["url"]
-
         await message.channel.send(image_url)
 
-    if message.content.startswith('-!speak'):
+    elif message.content.startswith('-!speak'):
         text = message.content
         text = text.replace("-!speak", "")
-
         result = requests.post(
             "https://api.deepai.org/api/text-generator",
             data={
@@ -40,15 +39,14 @@ async def on_message(message):
         json_result = result.json()
         await message.channel.send(json_result["output"])
 
-    if message.content.startswith("-!private"):
+    elif message.content.startswith("-!private"):
         await message.delete(delay=2)
 
-    if message.content.startswith("-!fact"):
+    elif message.content.startswith("-!fact"):
         dogFacts = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1"
         trivia = "https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=boolean&encode=base64"
 
         response = requests.get(trivia)
-
 
         trivia_object = response.json()
 
@@ -59,8 +57,6 @@ async def on_message(message):
         fact = fact.decode('utf-8')
         fact = fact[:-1]
 
-        print(fact)
-
         accuracy = results_object[0]["correct_answer"]
         accuracy = base64.b64decode(accuracy).lower()
         accuracy = accuracy.decode('utf-8')
@@ -70,9 +66,22 @@ async def on_message(message):
         elif accuracy == "false":
             fact = f"Did you know that '{fact}' is not true?"
 
-        print(accuracy)
-
         await message.channel.send(fact)
+
+    elif message.content.startswith("-!animals"):
+        animals = ["cat", "dog", "wolf", "otter", "panda"]
+        command = f"a!{random.choice(animals)}"
+        await message.channel.send(command)
+
+    elif message.content.startswith("-!help"):
+        help_message = "Currently the commands that we have are are: \n 1) hello \n 2) cat \n 3) private \n 4) fact " \
+                       "\n 5) animals "
+        await message.channel.send(help_message)
+
+    elif message.content.startswith("-!"):
+        response_text = "Looks like you are having some troubles, that is not a valid command. Try again! I believe " \
+                        "in you! "
+        await message.channel.send(response_text)
 
 
 client.run('ODI3OTcyMzU3MzY4MDUzODMw.YGizWA.08HUC_slOmNj65veuKenyt4oA40')
